d866d0d81bba7cc298ea964907cdd12ee1dfeb84
kind012 <tranthienduc1001@gmal.com>
2023-09-21 16:24:51 +0700

    change style intro section

--- src/layout/intro.tsx
+++ src/layout/intro.tsx
@@ -1,30 +1,38 @@
 import { motion } from "framer-motion";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
+
 const Intro = () => {
   const firstText = useRef<HTMLParagraphElement>(null);
   const secondText = useRef<HTMLParagraphElement>(null);
   let xPercent = 0;
   const direction = -1;
+
   const texts = [
     "TRAN THIEN DUC - UI/UX DESIGNER - FRONTEND DEVELOPER -",
     "TRAN THIEN DUC - UI/UX DESIGNER - FRONTEND DEVELOPER -",
-    "Front End",
-    "Developer",
-    "Portfolio/23.",
-    "Web developer",
-    "Web design",
-    "Portfolio 2023",
+    "FRONTEND",
+    "DEVELOPER",
+    "PORTFOLIO/23",
+    "INSTAGRAM",
+    "AVAILABLE FOR PERSON",
+    "WORK FROM SEP 28'",
   ];
 
   const variants = {
     hidden: {
-      y: "370%",
+      y: "270%",
       transition: { ease: [0.325, 0.04, 0.56, 0.96], duration: 0.95 },
     },
     visible: {
       y: 0,
-      transition: { ease: [0.475, 0.05, 0.57, 0.97], duration: 0.85 },
+      transition: {
+        ease: [0.475, 0.05, 0.57, 0.97],
+        duration: 0.8,
+        stagger: {
+          ammout: 0.4,
+        },
+      },
     },
   };
   useEffect(() => {
@@ -44,18 +52,13 @@ const Intro = () => {
   return (
     <section
       id="intro"
-      className="relative flex flex-col justify-center select-none h-screen md:mb-[195px]"
+      className="relative flex flex-col justify-center select-none lg:mb-[107px]"
     >
-      <div className="lg:px-[140px] lg:py-[152px] sm:px-[40px] pl-[25px] lg:w-full">
-        <div className="flex flex-col lg:flex-row lg:gap-[19rem] items-start lg:items-end overflow-hidden">
-          <div
-            className="font-semibold sm:text-3xl md:text lg:leading-[74px] leading-[50px] sm:w-fit lg:max-w-[678px] py-6 font-ClashGrotesk"
-            style={{
-              display: "block",
-            }}
-          >
+      <div className="lg:px-[47px] lg:py-[152px] sm:px-[40px] pl-[25px] lg:h-screen ">
+        <div className="flex flex-col lg:flex-row lg:gap-[19rem] items-start lg:items-end overflow-hidden mb-[10.5rem]">
+          <div className="lg:leading-[129px] font-semibold sm:text-3xl md:text sm:w-fit lg:max-w-[678px] py-6 ">
             <motion.h1
-              className="overflow-visible md:flex-1 lg:text-[71px] text-[63px]"
+              className="md:flex-1 lg:text-[160px] font-semibold text-[63px]"
               initial="hidden"
               animate="visible"
               variants={variants}
@@ -66,72 +69,86 @@ const Intro = () => {
               initial="hidden"
               animate="visible"
               variants={variants}
-              className="lg:text-[71px] text-[40px]"
+              className="lg:text-[160px] font-semibold text-[40px] lg:pl-[157px]"
             >
               {texts[3]}
             </motion.h1>
-            <motion.h1
+            <motion.span
               initial="hidden"
               animate="visible"
               variants={variants}
-              className="lg:text-[71px] text-[40px] decoration-black underline"
+              className="lg:text-[24px] lg:w-[231px] lg:pl-[53rem] leading-[52px] font-medium text-[40px] block "
             >
               {texts[4]}
-            </motion.h1>
-          </div>
-          <div className="flex flex-col gap-[32px] lg:max-w-[452px] ">
-            <motion.p
-              className="text-base lg:text-lg font-ClashGrotesk "
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-            >
-              I create{" "}
-              <span className="inline-block font-bold">user experiences</span>,
-              design beautiful websites for
-              <span className="inline-block font-bold"> businesses</span>, and
-              connect people through{" "}
-              <span className="inline-block font-bold">
-                design and development.
-              </span>
-            </motion.p>
-            <motion.div
-              className="font-normal text-base text-[#000000] mb-[45px] w-[245px] overflow-hidden font-ClashGrotesk border border-[#909090] rounded-full px-2 py-3 text-center flex"
-              initial="hidden"
-              animate="visible"
-              variants={variants}
-            >
-              <motion.div
-                className="relative flex whitespace-nowrap font-ClashGrotesk"
-                initial="hidden"
-                animate="visible"
-                variants={variants}
-              >
-                <p ref={firstText}>{texts[0]}</p>
-                <p ref={secondText} className=" absoluteleft-full">
-                  {texts[1]}
-                </p>
-              </motion.div>
-            </motion.div>
+            </motion.span>
           </div>
         </div>
-      </div>
-      <div
-        className="px-6 font-medium md:px-20 font-ClashGrotesk"
-        style={{
-          overflow: "hidden",
-          display: "inline-block",
-        }}
-      >
-        <motion.p initial="hidden" animate="visible" variants={variants}>
-          {texts[5]}
-        </motion.p>
-        <motion.p initial="hidden" animate="visible" variants={variants}>
-          {texts[6]}
-        </motion.p>
-        <motion.p initial="hidden" animate="visible" variants={variants}>
-          {texts[7]}
-        </motion.p>
+        <div className="flex items-center justify-between gap-8 lg:flex-row">
+          <motion.div
+            className="font-normal text-base text-[#000000]  w-[245px] overflow-hidden font-ClashGrotesk flex"
+            initial="hidden"
+            animate="visible"
+            variants={variants}
+          >
+            <motion.div
+              className="relative flex whitespace-nowrap font-ClashGrotesk"
+              initial="hidden"
+              animate="visible"
+              variants={variants}
+            >
+              <p ref={firstText}>{texts[0]}</p>
+              <p ref={secondText} className="absolute left-full">
+                {texts[1]}
+              </p>
+            </motion.div>
+          </motion.div>
+          <div>
+            <motion.a
+              href="https://www.instagram.com/tranthienducc/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-[15px] font-medium flex items-center gap-1"
+              initial="hidden"
+              animate="visible"
+              variants={variants}
+            >
+              {texts[5]}
+              <span className="transition duration-300 ease-in-out hover:rotate-45">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"
+                  />
+                </svg>
+              </span>
+            </motion.a>
+          </div>
+          <div
+            className="px-6 font-medium font-ClashGrotesk"
+            style={{
+              overflow: "hidden",
+            }}
+          >
+            <motion.p initial="hidden" animate="visible" variants={variants}>
+              {texts[6]}
+            </motion.p>
+            <motion.p
+              initial="hidden"
+              animate="visible"
+              variants={variants}
+              className=""
+            >
+              {texts[7]}
+            </motion.p>
+          </div>
+        </div>
       </div>
     </section>
   );
